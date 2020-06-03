748c3208d7c9d8d2d2456249660906f2a3664347
jibril2333 <slc2521937387@outlook.com>
2020-06-03 13:39:28 +0800

    commit

---
+++ SE/config.py
@@ -0,0 +1,11 @@
+
+DIALECT = "mysql"
+DRIVER = "mysqldb"
+USERNAME = "root"
+PASSWORD = "123456"
+HOST = "127.0.0.1"
+PORT = "3306"
+DATABASE = "supermarket"
+
+SQLALCHEMY_DATABASE_URI = "{}+{}://{}:{}@{}:{}/{}?charset=utf8".format(DIALECT, DRIVER, USERNAME, PASSWORD, HOST, PORT, DATABASE)
+SQLALCHEMY_TRACK_MODIFICATIONS = False
